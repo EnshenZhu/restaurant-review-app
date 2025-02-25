d796376a71e01f5f2f93bd3673954a6070438c37
EnshenZhu <enshen@uoguelph.ca>
2025-02-24 19:01:55 -0500

    fix the db.js typo

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(process.env.MONGO_URI, {
+const client = new MongoClient(process.env.MONGO_URL, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
