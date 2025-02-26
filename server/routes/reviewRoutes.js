352324f1fd9cfa17a7872fcda80055380dfb6816
EnshenZhu <enshen@uoguelph.ca>
2025-02-26 13:09:36 -0500

    add additional menu and review routes

--- server/routes/reviewRoutes.js
+++ server/routes/reviewRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const reviewController = require('../controllers/reviewController');
-// If using Multer for image upload:
-// const upload = require('../middleware/upload');
+
+// use Multer for image upload:
+const upload = require('../middleware/upload');
 
 router.get('/', reviewController.getAllReviews);
-// router.post('/', upload.single('image'), reviewController.createReview);
+router.post('/', upload.single('image'), reviewController.createReview);
 router.post('/', reviewController.createReview);
 
 module.exports = router;
