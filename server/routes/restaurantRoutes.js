9280378800791ebbe559bdee4a8c2f168b7fedcc
EnshenZhu <enshen@uoguelph.ca>
2025-02-25 20:03:00 -0500

    re-structure the backend and frontend

--- server/routes/restaurantRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getRestaurants } = require('../controllers/restaurantController');
-
-router.get('/', getRestaurants);
-
-module.exports = router;
--- server/routes/reviewRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { uploadImage } = require('../controllers/reviewController');
-
-router.post('/upload', uploadImage);
-
-module.exports = router;
